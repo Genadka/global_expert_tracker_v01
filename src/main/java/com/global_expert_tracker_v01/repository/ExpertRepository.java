74d962517884587d1a585ed86bc96c27eddabdcd
Genadka <Badyamovich@gmail.com>
2025-05-03 22:05:16 +0300

    небольшие правки

--- src/main/java/com/global_expert_tracker_v01/repository/ExpertRepository.java
+++ src/main/java/com/global_expert_tracker_v01/repository/ExpertRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 @Repository
 public interface ExpertRepository extends JpaRepository<Expert, Long> {
 
-    //нужен лист экспертов которые свободны на запрашиваемую дату и которые свободны на предыдущий день
-    //и которые рядом с местом выезда
+    //нужен лист экспертов которые свободны на запрашиваемую дату и на предыдущий день
+    //и рядом с местом выезда
 }
